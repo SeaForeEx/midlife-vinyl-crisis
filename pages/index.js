d39005f7ae8e6caca7dcb9aa3d75e3a84c3f625e
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-07-19 19:56:30 -0500

    All Postman calls work, setup NavBar and basic pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
-
 function Home() {
-  const { user } = useAuth();
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -14,12 +9,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      <h1>Welcome to Midlife Vinyl Crisis!</h1>
+      <p>Check Out Our New Releases!</p>
     </div>
   );
 }
---
+++ pages/records/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Records() {
+  return <div>Records is cool</div>;
+}
